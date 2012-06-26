4942e3bf337fe005f48d779c7b0adfe3216d1750
Carl Crowder <github@carlcrowder.com>
2012-06-26 17:04:39 +0200

    Adding settings for unit tests

--- gubbins/db/tests/field_tests.py
+++ gubbins/db/tests/field_tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 class EnumFieldTest(TestCase):
     
     def test_YOUR_FACE(self):
-        pass
+        print 'yo'
---
+++ gubbins/tests/settings.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+        'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'gubbins-db.sqlite',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+        }
+}
