edcb7653a1e17783613ea77b9af950cc9a263efa
carlio <git@carlcrowder.com>
2012-10-31 23:29:35 +0100

    version is now "dev" again

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 # -*- coding: UTF-8 -*-
 from distutils.core import setup
 from setuptools import find_packages
+import time
 
-_version = "1.0.2"
+_version = "0.%s.dev" % int(time.time())
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
     
 # common dependencies
