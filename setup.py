082e8e6e48fc677e22799ad17da907dceb365be1
carlio <git@carlcrowder.com>
2012-11-30 09:10:45 +0100

    fixing version to be pep386 compliant for dev version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import find_packages
 import time
 
-_version = "0.%s.dev" % int(time.time())
+_version = "1.1.dev%s" % int(time.time())
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
     
 # common dependencies
